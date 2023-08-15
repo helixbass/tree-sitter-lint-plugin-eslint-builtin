ce13f879f800fdd9c8c70f4fc0e393f07586c136
Julian Rosse <julian@helixbass.net>
2023-08-15 08:07:16 -0400

    working on no unreachable loop

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -30,8 +30,8 @@ use rules::{
     no_octal_escape_rule, no_octal_rule, no_plusplus_rule, no_proto_rule,
     no_restricted_properties_rule, no_return_assign_rule, no_script_url_rule, no_self_assign_rule,
     no_sequences_rule, no_ternary_rule, no_throw_literal_rule, no_unneeded_ternary_rule,
-    no_unreachable_rule, no_unused_labels_rule, no_useless_call_rule, no_useless_catch_rule,
-    no_useless_return_rule, require_yield_rule, sort_keys_rule,
+    no_unreachable_loop_rule, no_unreachable_rule, no_unused_labels_rule, no_useless_call_rule,
+    no_useless_catch_rule, no_useless_return_rule, require_yield_rule, sort_keys_rule,
 };
 
 pub fn instantiate() -> Plugin {
@@ -91,6 +91,7 @@ pub fn instantiate() -> Plugin {
             no_useless_return_rule(),
             no_self_assign_rule(),
             constructor_super_rule(),
+            no_unreachable_loop_rule(),
         ],
         event_emitter_factories: vec![],
     }
