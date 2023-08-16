7d025d06018a7c2e3316e2d0b35bb75b40187096
Julian Rosse <julian@helixbass.net>
2023-08-16 16:01:36 -0400

    working on no unsafe optional chaining

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -31,8 +31,9 @@ use rules::{
     no_restricted_properties_rule, no_return_assign_rule, no_script_url_rule, no_self_assign_rule,
     no_sequences_rule, no_ternary_rule, no_this_before_super_rule, no_throw_literal_rule,
     no_unneeded_ternary_rule, no_unreachable_loop_rule, no_unreachable_rule,
-    no_unsafe_finally_rule, no_unsafe_negation_rule, no_unused_labels_rule, no_useless_call_rule,
-    no_useless_catch_rule, no_useless_return_rule, require_yield_rule, sort_keys_rule,
+    no_unsafe_finally_rule, no_unsafe_negation_rule, no_unsafe_optional_chaining_rule,
+    no_unused_labels_rule, no_useless_call_rule, no_useless_catch_rule, no_useless_return_rule,
+    require_yield_rule, sort_keys_rule,
 };
 
 pub fn instantiate() -> Plugin {
@@ -97,6 +98,7 @@ pub fn instantiate() -> Plugin {
             no_this_before_super_rule(),
             no_unsafe_finally_rule(),
             no_unsafe_negation_rule(),
+            no_unsafe_optional_chaining_rule(),
         ],
         event_emitter_factories: vec![],
     }
