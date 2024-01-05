6f0c5775f3be72df46184f18fad69770e1db1862
Julian Rosse <julian@helixbass.net>
2024-01-05 08:24:29 -0500

    working on prefer promise reject errors

--- plugin/src/rules/prefer_promise_reject_errors.rs
+++ plugin/src/rules/prefer_promise_reject_errors.rs
@@ -1,12 +1,25 @@
 use std::sync::Arc;
 
+use itertools::Itertools;
 use serde::Deserialize;
-use tree_sitter_lint::{rule, violation, Rule};
+use squalid::OptionExt;
+use tree_sitter_lint::{rule, tree_sitter::Node, violation, NodeExt, QueryMatchContext, Rule};
+
+use crate::{ast_helpers::{get_call_expression_arguments, get_function_params}, kind::{Undefined, Identifier, CallExpression}, utils::ast_utils, scope::ScopeManager};
 
 #[derive(Default, Deserialize)]
 #[serde(default)]
 struct Options {
     allow_empty_reject: bool,
+}
+
+fn is_promise_reject_call(node: Node, context: &QueryMatchContext) -> bool {
+    ast_utils::is_specific_member_access(
+        node.field("function"),
+        Some("Promise"),
+        Some("reject"),
+        context,
+    )
 }
 
 pub fn prefer_promise_reject_errors_rule() -> Arc<dyn Rule> {
@@ -21,15 +34,70 @@ pub fn prefer_promise_reject_errors_rule() -> Arc<dyn Rule> {
             [per-config]
             allow_empty_reject: bool = options.allow_empty_reject,
         },
+        methods => {
+            fn check_reject_call(&self, call_expression: Node, context: &QueryMatchContext) {
+                let Some(args) = get_call_expression_arguments(call_expression) else {
+                    return;
+                };
+                let args = args.collect_vec();
+                if args.is_empty() && self.allow_empty_reject {
+                    return;
+                }
+                if args.is_empty() ||
+                    !ast_utils::could_be_error(args[0], context) ||
+                    args[0].kind() == Undefined {
+                    context.report(violation! {
+                        node => call_expression,
+                        message_id => "reject_an_error",
+                    });
+                }
+            }
+        },
         listeners => [
-            r#"(
-              (debugger_statement) @c
-            )"# => |node, context| {
-                context.report(violation! {
-                    node => node,
-                    message_id => "unexpected",
-                });
+            r#"
+              (call_expression
+                function: (member_expression
+                  object: (identifier) @promise (#eq? @promise "Promise")
+                )
+              ) @call_expression
+            "# => |captures, context| {
+                let node = captures["call_expression"];
+                if !is_promise_reject_call(node, context) {
+                    return;
+                }
+                self.check_reject_call(node, context);
             },
+            r#"
+              (new_expression
+                constructor: (identifier) @promise (#eq? @promise "Promise")
+              ) @new_expression
+            "# => |captures, context| {
+                let node = captures["new_expression"];
+                let Some(first_arg) = get_call_expression_arguments(node).unwrap().next().filter(|&arg| {
+                    ast_utils::is_function(arg) &&
+                        get_function_params(arg).nth(1).matches(|param| param.kind() == Identifier)
+                }) else {
+                    return;
+                };
+                let scope_manager = context.retrieve::<ScopeManager<'a>>();
+                scope_manager.get_declared_variables(first_arg)
+                    .find(|variable| {
+                        variable.name() == &*get_function_params(first_arg).nth(1).unwrap().text(context)
+                    })
+                    .unwrap()
+                    .references()
+                    .filter(|ref_| ref_.is_read())
+                    .filter(|ref_| {
+                        ref_.identifier().parent().unwrap().kind() == CallExpression &&
+                            ref_.identifier() == ref_.identifier().parent().unwrap().field("function")
+                    })
+                    .for_each(|ref_| {
+                        self.check_reject_call(
+                            ref_.identifier().parent().unwrap(),
+                            context,
+                        );
+                    });
+            }
         ],
     }
 }
