7e00405e8fc9a0294ffd834625591b99e999764a
Julian Rosse <julian@helixbass.net>
2024-01-05 13:37:51 -0500

    working on prefer destructuring

--- plugin/src/rules/prefer_destructuring.rs
+++ plugin/src/rules/prefer_destructuring.rs
@@ -1,7 +1,21 @@
 use std::sync::Arc;
 
+use once_cell::sync::Lazy;
 use serde::Deserialize;
-use tree_sitter_lint::{rule, violation, Rule};
+use squalid::{EverythingExt, OptionExt};
+use tree_sitter_lint::{
+    rule, tree_sitter::Node, violation, Fixer, NodeExt, QueryMatchContext, Rule,
+};
+
+use crate::{
+    ast_helpers::{get_number_literal_value, Number, NumberOrBigInt},
+    kind,
+    kind::{
+        AssignmentExpression, Identifier, Kind, MemberExpression, PrivatePropertyIdentifier,
+        SubscriptExpression, Super, VariableDeclarator,
+    },
+    utils::{ast_utils, ast_utils::get_static_string_value},
+};
 
 #[derive(Copy, Clone, Default, Deserialize)]
 #[serde(default)]
@@ -80,6 +94,97 @@ impl Default for Options {
     }
 }
 
+static PRECEDENCE_OF_ASSIGNMENT_EXPR: Lazy<u32> =
+    Lazy::new(|| ast_utils::get_kind_precedence(AssignmentExpression));
+
+fn is_array_index_access(node: Node, context: &QueryMatchContext) -> bool {
+    node.kind() == SubscriptExpression
+        && node.field("index").thrush(|index| {
+            index.kind() == kind::Number
+                && matches!(
+                    get_number_literal_value(index, context),
+                    NumberOrBigInt::Number(Number::Integer(_))
+                )
+        })
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum ArrayOrObject {
+    Array,
+    Object,
+}
+
+fn report(
+    report_node: Node,
+    type_: ArrayOrObject,
+    fix: Option<impl Fn(&mut Fixer)>,
+    context: &QueryMatchContext,
+) {
+    context.report(violation! {
+        node => report_node,
+        message_id => "prefer_destructuring",
+        data => {
+            type => match type_ {
+                ArrayOrObject::Array => "array",
+                ArrayOrObject::Object => "object",
+            },
+        },
+        fix => move |fixer| {
+            if let Some(fix) = fix.as_ref() {
+                fix(fixer);
+            }
+        }
+    });
+}
+
+fn should_fix(node: Node, context: &QueryMatchContext) -> bool {
+    if node.kind() != VariableDeclarator {
+        return false;
+    }
+    let id = node.field("name");
+    if id.kind() != Identifier {
+        return false;
+    }
+    let init = node.field("value");
+    if init.kind() != MemberExpression {
+        return false;
+    }
+    let init_property = init.field("property");
+    if init_property.kind() != Identifier {
+        return false;
+    }
+    id.text(context) == init_property.text(context)
+}
+
+fn fix_into_object_destructuring<'a>(
+    fixer: &mut Fixer,
+    node: Node<'a>,
+    context: &QueryMatchContext<'a, '_>,
+) {
+    let right_node = node.field("value");
+
+    let right_node_object = right_node.field("object");
+    if context.get_comments_inside(node).count()
+        > context.get_comments_inside(right_node_object).count()
+    {
+        return;
+    }
+
+    let mut object_text = right_node_object.text(context);
+
+    if ast_utils::get_precedence(right_node_object) < *PRECEDENCE_OF_ASSIGNMENT_EXPR {
+        object_text = format!("({object_text})").into();
+    }
+
+    fixer.replace_text(
+        node,
+        format!(
+            "{{{}}} = {object_text}",
+            right_node.field("property").text(context)
+        ),
+    );
+}
+
 pub fn prefer_destructuring_rule() -> Arc<dyn Rule> {
     rule! {
         name => "no-debugger",
@@ -94,14 +199,84 @@ pub fn prefer_destructuring_rule() -> Arc<dyn Rule> {
             enforce_for_renamed_properties: bool = options.enforce_for_renamed_properties(),
             normalized_options: ByNodeType = options.normalized_options(),
         },
+        methods => {
+            fn should_check(&self, node_type: Kind, destructuring_type: ArrayOrObject) -> bool {
+                match node_type {
+                    VariableDeclarator => self.normalized_options.variable_declarator,
+                    AssignmentExpression => self.normalized_options.assignment_expression,
+                    _ => unreachable!(),
+                }.matches(|array_and_object| {
+                    match destructuring_type {
+                        ArrayOrObject::Array => array_and_object.array,
+                        ArrayOrObject::Object => array_and_object.object,
+                    }
+                })
+            }
+
+            fn perform_check(&self, left_node: Node<'a>, right_node: Node<'a>, report_node: Node<'a>, context: &QueryMatchContext<'a, '_>) {
+                if !matches!(
+                    right_node.kind(),
+                    MemberExpression | SubscriptExpression
+                ) {
+                    return;
+                }
+                if right_node.field("object").kind() == Super {
+                    return;
+                }
+                if right_node.kind() == MemberExpression &&
+                    right_node.field("property").kind() == PrivatePropertyIdentifier {
+                    return;
+                }
+
+                if is_array_index_access(right_node, context) {
+                    if self.should_check(report_node.kind(), ArrayOrObject::Array) {
+                        report(report_node, ArrayOrObject::Array, Option::<fn(&mut Fixer)>::None, context);
+                    }
+                    return;
+                }
+
+                let fix = should_fix(report_node, context).then_some(
+                    |fixer: &mut Fixer| {
+                        fix_into_object_destructuring(fixer, report_node, context);
+                    }
+                );
+
+                if self.should_check(report_node.kind(), ArrayOrObject::Object) &&
+                    self.enforce_for_renamed_properties {
+                    report(report_node, ArrayOrObject::Object, fix, context);
+                    return;
+                }
+
+                #[allow(clippy::collapsible_if)]
+                if self.should_check(report_node.kind(), ArrayOrObject::Object) {
+                    if match right_node.kind() {
+                        MemberExpression => left_node.text(context) == right_node.field("property").text(context),
+                        SubscriptExpression => right_node.field("index").thrush(|property| {
+                            property.kind() == kind::String &&
+                                left_node.text(context) == get_static_string_value(property, context).unwrap()
+                        }),
+                        _ => unreachable!(),
+                    } {
+                        report(report_node, ArrayOrObject::Object, fix, context);
+                    }
+                }
+            }
+        },
         listeners => [
-            r#"(
-              (debugger_statement) @c
-            )"# => |node, context| {
-                context.report(violation! {
-                    node => node,
-                    message_id => "unexpected",
-                });
+            r#"
+              (variable_declarator
+                value: [
+                  (member_expression)
+                  (subscript_expression)
+                ]
+              ) @c
+            "# => |node, context| {
+                self.perform_check(node.field("name"), node.field("value"), node, context);
+            },
+            r#"
+              (assignment_expression) @c
+            "# => |node, context| {
+                self.perform_check(node.field("left"), node.field("right"), node, context);
             },
         ],
     }
